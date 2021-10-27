2d7f64312f70a3f261cfb24b524eba75a2ea2e04
Luís Henrique Picinin Jandre <luishpj@outlook.com.br>
2021-10-26 22:42:05 -0300

    projeto desenvolvido nas aulas na versão java 17 LTS

---
+++ src/main/java/org/digitalinnovationone/gof/service/ClienteService.java
@@ -0,0 +1,17 @@
+package org.digitalinnovationone.gof.service;
+
+import org.digitalinnovationone.gof.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
